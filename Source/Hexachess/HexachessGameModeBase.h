7bf0df9cf43098ab9c85483bf21498519c804bb3
Ryan Ash <scarletspark@gmail.com>
2023-09-19 22:42:20 +0200

    gm check integration

--- Source/Hexachess/HexachessGameModeBase.h
+++ Source/Hexachess/HexachessGameModeBase.h
@@ -58,50 +58,11 @@ public:
 	UFUNCTION(BlueprintCallable)
 	virtual void MovePiece(FIntPoint From, FIntPoint To);
 
+	UFUNCTION(BlueprintCallable)
+	virtual bool IsCellUnderAttack(FIntPoint InPosition);
+
 private:
 
 	Board* ActiveBoard = nullptr;
 };
-
-
-// things to decide:
-// - Cell -> do we need it as Actor? or just a struct?
-// - notation -> later
-// - mapping visual board to logical:
-// --- visual board is generated
-// --- we store its details in a struct
-
-
-
-
-// cell: something that can be occupied by a piece
-// - position
-// - color id?..
-// - piece pointer?
-// - actor (component) pointer
-// piece: a base class for all pieces
-// - team id
-// - alive / dead
-// - piece type?..
-// - actor (component) pointer
-// board: a collection of cells and pieces
-
-
-
-
-// HexachessGameMode -> BP_HexachessGameMode
-// - assigns teams
-// - creates pieces
-// - creates logical board
-// - starts game
-
-// AHexGrid -> BP_HexGrid
-// - runs grid generation
-// - initializes cells
-
-// Controller
-// - presses on Piece -> Piece::GetPossibleMoves
-// - tries to move Piece -> Piece::IsValidMove
-// - moves Piece -> Piece::Move
-// -> returns move result meta struct: capture, check, checkmate, promotion (type), stalemate
 
